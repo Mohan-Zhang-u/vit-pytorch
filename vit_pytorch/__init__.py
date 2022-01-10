340048c29d8a9ae531c035cdc12bd9986f345872
Mohan Zhang <mohanzhang@cs.toronto.edu>
2022-01-09 20:01:54 -0500

    change ref name

--- vit_pytorch/__init__.py
+++ vit_pytorch/__init__.py
@@ -1,3 +1,3 @@
-from vit_pytorch.vit import ViT, ViTwithTextInput
+from vit_pytorch.vit import ViT, ViTwithTextInputChinese, ViTwithTextInputHorizontal
 from vit_pytorch.mae import MAE
 from vit_pytorch.dino import Dino
